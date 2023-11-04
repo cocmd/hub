b6ef298373e477d0a32e9a97d1dd71d3d7f462ed
mosh <mos@moshe>
2023-11-04 08:26:14 +0200

    fix homebrew file

--- packages/docker/scripts/dockerinstall.osx.sh
+++ packages/docker/scripts/dockerinstall.osx.sh
@@ -7,9 +7,6 @@ fi
 # Install Homebrew if not installed
 curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
 
-# Tap the Docker formulae
-brew tap docker/cli
-
 # Install Docker
 brew install docker
 
