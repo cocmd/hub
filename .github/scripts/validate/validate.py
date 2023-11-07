66e9b265ccce9d8c451c3fb34b55952db5a2d53c
moshe <moshe@mosh>
2023-11-08 00:02:44 +0200

    github actions

--- .github/scripts/validate/validate.py
+++ .github/scripts/validate/validate.py
@@ -1,6 +1,7 @@
 
 import os
 from typing import List, Optional
+import yaml
 
 
 def validate_package(path: str) -> Optional[str]:
@@ -15,6 +16,33 @@ def validate_package(path: str) -> Optional[str]:
     status = os.system(command)
     if status != 0:
         return f"Command \"\"failed with status {status}"
+    
+    # read cocmd.yaml file and check for the "name" field value 
+    # keep the value of "name" in a variable
+    # read the file with lib yaml
+    name = None
+    with open(f"{path}/cocmd.yaml", 'r') as stream:
+        try:
+            manifest = yaml.safe_load(stream)
+            name = manifest["name"]
+        except yaml.YAMLError as exc:
+            return f"Exception parsing YAML {str(exc)}"
+    
+    # now we run a check to see what was loaded
+    # if the package was loaded, then we are good
+    # in the "cocmd show package ..." output we must see
+    # at least the word "aliases", "PATH additions" or "automations"
+    # and also we must see "-location:"
+    
+    command = f"cocmd show package {name}"
+    output = os.popen(command).read()
+    print(output)
+    
+    if "aliases" not in output and "PATH additions" not in output and "automations" not in output:
+        return f"Package was not loaded. Please check the output of the command: {command}"
+    
+    if "location:" not in output:
+        return f"Package was not loaded. Please check the output of the command: {command}"
     
     
           
